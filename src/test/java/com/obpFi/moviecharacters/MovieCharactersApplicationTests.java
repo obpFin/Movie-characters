586e6fff34b3680426a4cb779a5d88d8a780b6fc
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-09-18 18:02:41 +0300

    first commit

---
+++ src/test/java/com/obpFi/moviecharacters/MovieCharactersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obpFi.moviecharacters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieCharactersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
